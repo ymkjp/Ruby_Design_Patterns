7c43e815bb4452af265e248156df70a74d008c05
ymkjp <lq33xxx@gmail.com>
2013-12-08 17:21:14 +0900

    Added some code to Adapter pattern

--- Adapter/Render.rb
+++ Adapter/Render.rb
@@ -6,6 +6,7 @@ class Render
         color = text_object.color
 
         # Show strings
+        puts "#{text} の靴は #{size} インチで #{color} 色だ"
     end
 end
 
@@ -19,18 +20,53 @@ class TextObject
     end
 end
 
+# Textobject とは異なったインターフェースをもつオブジェクト
 class BritishTextObject
     attr_reader :string, :size_mm, :colour
+
     def initialize(string, size_mm, colour)
         @string = string
         @size_mm = size_mm
-        @coluor = colour
+        @colour = colour
     end
 
     # ...
 end
 
-bto = BritishTextObject.new('hello', 50.8, :blue)
+# Adapter パターンを使って解決
+class BritishTextObjectAdapter < TextObject
+    def initialize(bto)
+        @bto = bto
+    end
+
+    def text
+        return @bto.string
+    end
+
+    def size_inches
+        return @bto.size_mm / 25.4
+    end
+
+    def color
+        return @bto.colour
+    end
+end
+
+text = BritishTextObjectAdapter.new(
+    BritishTextObject.new('やまもと', 28, :blue)
+)
+Render.new.render(text)
+
+class BritishTextObject
+    attr_reader :string, :size_mm, :colour
+    def initialize(string, size_mm, colour)
+        @string = string
+        @size_mm = size_mm
+        @colour = colour
+    end
+end
+
+bto = BritishTextObject.new('bto-san', 50.8, :blue)
 
 # インスタンスごとにメソッドを追加できる
 # オブジェクトに固有なメソッドを「特異メソッド (Singleton Method)」と呼ぶ
@@ -47,6 +83,7 @@ class << bto
         return size_mm/25.4
     end
 end
+Render.new.render(bto)
 
 # すべてのインスタンスにメソッド追加を適用したい場合はクラスに追加することもできる
 class BritishTextObject
@@ -62,6 +99,7 @@ class BritishTextObject
         return size_mm/25.4
     end
 end
+Render.new.render(BritishTextObject.new('All', 50.8, :blue))
 
 # こういった方法は強力で単純ですが、複雑な処理を行う場合や対象のクラスのことをよく
 # 知らない場合はアダプタをつくったほうがいい
